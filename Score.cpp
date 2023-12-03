7cc02d4da5c96ed7786133806bd4d440e11f5f3f
SadCivic <122512802+SadCivic@users.noreply.github.com>
2023-12-03 15:34:09 +0500

    Update Score.cpp

--- Score.cpp
+++ Score.cpp
@@ -4,13 +4,13 @@
 void Score::scoreInc(){
     score++;
 }
-
+//current score value is compared everytime to the existing high score value
 void Score::setHighscore(const double x){
     if (x>=highscore){
         highscore=x;
     }
 }
-        
+//the following are standard getters for both the score and the high score      
 double Score::getScore() const{
     return score;
 }
