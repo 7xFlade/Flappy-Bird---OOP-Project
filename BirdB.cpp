8a30b9c2842d0903b674cd06ddccebe20bc7a852
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-04 21:56:57 +0500

    Update BirdB.cpp

--- BirdB.cpp
+++ BirdB.cpp
@@ -1,5 +1,6 @@
 #include "BirdB.hpp"
 #include "TextureManager.hpp"
+// The Render method is used to render the bird based on its frame rate
 void BirdB::Render(SDL_Renderer* ren){
     
     frate++;
@@ -22,10 +23,11 @@ void BirdB::Render(SDL_Renderer* ren){
 //polymorphic behaviour
 //we need two more create texture operation since one instance of the bird is already instantiated when the game loads. After that we only have two more pictures to cater out of the four for BirdB
 }
+
+// createTexture1 and createTexture2 methods are used to create additional textures
 void BirdB::createTexture1(const char* address, SDL_Renderer* ren){
     Tex1=TextureManager::Texture(address, ren);
 }
 void BirdB::createTexture2(const char* address, SDL_Renderer* ren){
     Tex2=TextureManager::Texture(address, ren);
 }
-
