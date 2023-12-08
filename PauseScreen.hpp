db9c7ddc8bbd14af49d88344f3eb94bc64c5c669
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:15:44 +0500

    Update PauseScreen.hpp

--- PauseScreen.hpp
+++ PauseScreen.hpp
@@ -1,13 +1,21 @@
 #pragma once
-#include<SDL.h>
-#include<SDL_image.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include "Background.hpp"
 
 class PauseScreen
 {
-    public:
-        Background paused; 
-    public:
-        void Initialize(SDL_Renderer* ren);
-        void Render(SDL_Renderer* ren);
+public:
+    Background paused; // Background object for the pause screen
+
+public:
+    // Function to initialize the pause screen, loads background texture
+    // Parameters:
+    //   ren: The SDL_Renderer used for rendering
+    void Initialize(SDL_Renderer* ren);
+
+    // Function to render the pause screen on the provided renderer
+    // Parameters:
+    //   ren: The SDL_Renderer used for rendering
+    void Render(SDL_Renderer* ren);
 };
