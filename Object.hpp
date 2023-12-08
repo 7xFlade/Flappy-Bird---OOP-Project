ac8f8c2edab4415ad6c58b3bc18b635a8454e1da
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:24:48 +0500

    Update Object.hpp

--- Object.hpp
+++ Object.hpp
@@ -3,20 +3,46 @@
 #include <SDL_image.h>
 #include <iostream>
 
-
 class Object
 {
-    private:
-        SDL_Rect src,dest;
-        SDL_Texture* Tex;
-    public:
-        Object();
-        void setSource(int x, int y, int w, int h);
-        void setDest(int x, int y, int w, int h);
-        SDL_Texture* getTexture();
-        SDL_Rect& getSrc();
-        SDL_Rect& getDest();
-        void CreateTexture(const char* address, SDL_Renderer* ren);
-        void virtual Render(SDL_Renderer* ren)=0;
+private:
+    SDL_Rect src;       // Source rectangle for texture rendering
+    SDL_Rect dest;      // Destination rectangle for texture rendering
+    SDL_Texture* Tex;   // Texture for the object
 
+public:
+    // Default constructor for the Object class
+    Object();
+
+    // Setter function to set the source rectangle parameters
+    // Parameters:
+    //   x, y: Top-left corner coordinates of the source rectangle
+    //   w, h: Width and height of the source rectangle
+    void setSource(int x, int y, int w, int h);
+
+    // Setter function to set the destination rectangle parameters
+    // Parameters:
+    //   x, y: Top-left corner coordinates of the destination rectangle
+    //   w, h: Width and height of the destination rectangle
+    void setDest(int x, int y, int w, int h);
+
+    // Getter function to retrieve the texture associated with the object
+    SDL_Texture* getTexture();
+
+    // Getter function to retrieve the source rectangle
+    SDL_Rect& getSrc();
+
+    // Getter function to retrieve the destination rectangle
+    SDL_Rect& getDest();
+
+    // Function to load a texture from a file and set it as the object's texture
+    // Parameters:
+    //   address: File address of the texture image
+    //   ren: SDL_Renderer used for rendering
+    void CreateTexture(const char* address, SDL_Renderer* ren);
+
+    // Pure virtual function for rendering the object on the provided renderer
+    // Parameters:
+    //   ren: SDL_Renderer used for rendering
+    virtual void Render(SDL_Renderer* ren) = 0;
 };
