57a98c4f874c0f1239639921401e183bcfd1663d
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:29:01 +0500

    Update Nueron.cpp

--- Nueron.cpp
+++ Nueron.cpp
@@ -1,16 +1,21 @@
-#include "Nueron.hpp"
+#include "Neuron.hpp"
 
+// Default constructor for the Neuron class
 Neuron::Neuron()
 {
-	value = 0;
+    value = 0; // Initialize the value to 0
 }
 
+// Setter function to set the value of the neuron
+// Parameters:
+//   val: The value to set for the neuron
 void Neuron::setValue(double val)
 {
-	this->value = val;
+    this->value = val; // Set the value of the neuron
 }
 
+// Destructor for the Neuron class
 Neuron::~Neuron()
 {
-	value = 0;
+    value = 0; // Reset the value to 0 in the destructor
 }
