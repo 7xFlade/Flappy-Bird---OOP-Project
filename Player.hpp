127c362a2113d1402ce6bd5e9a255a506c7fc78c
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:13:30 +0500

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,16 +1,26 @@
 #pragma once
 #include "Object.hpp"
 
-class Player:public Object
-{
-    private:
-        double gravity=250;
-        double speed=0;
-        int time=0;
-    public:
-        void Update();
-        void Gravity();
-        double getYPos ();//const
-        virtual void Render(SDL_Renderer* ren)=0;
-        
+class Player : public Object {
+private:
+    double gravity = 250; // Gravity affecting the player
+    double speed = 0;     // Speed of the player's movement
+    int time = 0;         // Time variable (not specified for what purpose)
+
+public:
+    // Function to update the player's state
+    void Update();
+
+    // Function to apply gravity to the player
+    void Gravity();
+
+    // Getter function to retrieve the Y position of the player
+    // Returns:
+    //   The Y position of the player
+    double getYPos(); // const
+
+    // Pure virtual function to render the player on the screen
+    // Parameters:
+    //   ren: The SDL_Renderer used for rendering
+    virtual void Render(SDL_Renderer* ren) = 0;
 };
