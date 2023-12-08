e12fb0938f857ccaf915cfab38e87de265fe6c28
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:30:53 +0500

    Update NeuralNetwork.cpp

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -1,30 +1,38 @@
-#include "NueralNetwork.hpp"
+#include "NeuralNetwork.hpp"
 #include <SDL.h>
 
+// Default constructor for the NeuralNetwork class
 NeuralNetwork::NeuralNetwork()
 {
-	threshold = 0;
-	subDivisor = 800;
-	probablity = 7;
+    threshold = 0;       // Initialize threshold to 0
+    subDivisor = 800;    // Initialize subDivisor to 800
+    probability = 7;     // Initialize probability to 7
 }
 
-
+// Function to set input values for the neural network's input layer
+// Parameters:
+//   birdY: Y-coordinate of the bird in the game
+//   CheckPointY: Y-coordinate of the checkpoint in the game
 void NeuralNetwork::InputLayer(int birdY, int CheckPointY)
 {
-	InputNeuron1.value = birdY;
-	InputNeuron2.value = CheckPointY;
-	threshold = CheckPointY;
+    InputNeuron1.value = birdY;       // Set the value of the first input neuron to the bird's Y-coordinate
+    InputNeuron2.value = CheckPointY; // Set the value of the second input neuron to the checkpoint's Y-coordinate
+    threshold = CheckPointY;          // Update the threshold based on the checkpoint's Y-coordinate
 }
 
-
+// Function to update the neural network based on inputs
+// Parameters:
+//   birdY: Y-coordinate of the bird in the game
+//   CheckPointY: Y-coordinate of the checkpoint in the game
 void NeuralNetwork::Update(int birdY, int CheckPointY)
 {
-	InputLayer(birdY, CheckPointY);
+    InputLayer(birdY, CheckPointY);   // Call the InputLayer function to set input values
 }
 
-
+// Destructor for the NeuralNetwork class
 NeuralNetwork::~NeuralNetwork()
 {
-	threshold = 0;
-	subDivisor = 0;
+    threshold = 0;       // Reset threshold to 0 in the destructor
+    subDivisor = 0;      // Reset subDivisor to 0 in the destructor
+    probability = 0;     // Reset probability to 0 in the destructor
 }
