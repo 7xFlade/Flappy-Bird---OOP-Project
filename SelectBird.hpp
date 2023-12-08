a3ed35e5cab6acedb52ba0a357a3343ffd4076c1
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:10:53 +0500

    Update SelectBird.hpp

--- SelectBird.hpp
+++ SelectBird.hpp
@@ -1,19 +1,30 @@
 #pragma once
-#include<SDL.h>
-#include<SDL_image.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include "Background.hpp"
 
 class SelectBird
 {
-    private:
-        bool isChosenB;
-        Background m;
-        int bird;
-    public:
-        SelectBird();
-        void Initialize(SDL_Renderer* ren);
-        int EventHandling(SDL_Event& b);
-        void Render(SDL_Renderer* ren);
-        bool getChosenB();
-        int getBird();
+private:
+    bool isChosenB;  // Flag to indicate whether a bird is chosen
+    Background m;    // Background object for rendering
+    int bird;        // Index representing the selected bird
+public:
+    // Constructor to initialize the SelectBird object
+    SelectBird();
+
+    // Function to initialize the SelectBird object, takes an SDL_Renderer for rendering
+    void Initialize(SDL_Renderer* ren);
+
+    // Function to handle events related to bird selection, returns the selected bird index
+    int EventHandling(SDL_Event& b);
+
+    // Function to render the bird selection screen on the provided renderer
+    void Render(SDL_Renderer* ren);
+
+    // Getter function to retrieve the status of whether a bird is chosen
+    bool getChosenB();
+
+    // Getter function to retrieve the index of the selected bird
+    int getBird();
 };
