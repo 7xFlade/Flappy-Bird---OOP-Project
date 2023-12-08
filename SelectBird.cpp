e615a390089ddc5957b501f71cd4ea991fcc63fa
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:11:34 +0500

    Update SelectBird.cpp

--- SelectBird.cpp
+++ SelectBird.cpp
@@ -1,53 +1,56 @@
 #include "SelectBird.hpp"
 
-SelectBird::SelectBird(){
-    isChosenB=false;
+// Constructor to initialize SelectBird object
+SelectBird::SelectBird() {
+    isChosenB = false;
 }
 
-void SelectBird::Initialize(SDL_Renderer* ren){
-    
+// Function to initialize SelectBird, loads background texture
+void SelectBird::Initialize(SDL_Renderer* ren) {
     m.CreateTexture("image/sb.png", ren);
 }
 
-int SelectBird::EventHandling(SDL_Event& b)
-{
-	//short summary of the following
-	//we are basically using SDL operations to figure out where on the screen the user is clicking
-	//on the screen, there are three different sprites or birds available
-	//depending on where the user is clicking, we can detemine their choice of sprite and load it
-	SDL_PollEvent(&b);
-	if (b.type == SDL_QUIT)
-	{
-		return -1;
-	}
-	else if (b.type == SDL_MOUSEBUTTONDOWN && b.motion.x > 100 && b.motion.x < 250 && b.motion.y > 280 && b.motion.y < 350)
-	{
-		bird=1;
-		isChosenB = true;
-	}
-	else if (b.type == SDL_MOUSEBUTTONDOWN && b.motion.x > 350 && b.motion.x < 500 && b.motion.y > 280 && b.motion.y < 350)
-	{
-		bird=2;
-		isChosenB = true;
-	}
-	else if (b.type == SDL_MOUSEBUTTONDOWN && b.motion.x > 550 && b.motion.x < 730 && b.motion.y > 280 && b.motion.y < 350)
-	{
-		bird=3;
-		isChosenB = true;
-	}
-	return 0;
+// Function to handle mouse events for bird selection
+// Returns: -1 if SDL_QUIT event occurs, 0 otherwise
+int SelectBird::EventHandling(SDL_Event& b) {
+    // Poll for SDL events
+    SDL_PollEvent(&b);
+
+    // Check for SDL_QUIT event
+    if (b.type == SDL_QUIT) {
+        return -1;
+    }
+    // Check if the user clicked on the first bird
+    else if (b.type == SDL_MOUSEBUTTONDOWN && b.motion.x > 100 && b.motion.x < 250 && b.motion.y > 280 && b.motion.y < 350) {
+        bird = 1;
+        isChosenB = true;
+    }
+    // Check if the user clicked on the second bird
+    else if (b.type == SDL_MOUSEBUTTONDOWN && b.motion.x > 350 && b.motion.x < 500 && b.motion.y > 280 && b.motion.y < 350) {
+        bird = 2;
+        isChosenB = true;
+    }
+    // Check if the user clicked on the third bird
+    else if (b.type == SDL_MOUSEBUTTONDOWN && b.motion.x > 550 && b.motion.x < 730 && b.motion.y > 280 && b.motion.y < 350) {
+        bird = 3;
+        isChosenB = true;
+    }
+
+    // Return 0 to indicate that no SDL_QUIT event occurred
+    return 0;
 }
 
-void SelectBird::Render(SDL_Renderer* ren)
-{
-	m.Render(ren);
+// Function to render the background texture
+void SelectBird::Render(SDL_Renderer* ren) {
+    m.Render(ren);
 }
 
-bool SelectBird::getChosenB()
-{
-	return isChosenB;
+// Getter function to retrieve the status of whether a bird is chosen
+bool SelectBird::getChosenB() {
+    return isChosenB;
 }
 
-int SelectBird::getBird(){
-	return bird;
+// Getter function to retrieve the index of the selected bird
+int SelectBird::getBird() {
+    return bird;
 }
