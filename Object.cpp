5402dc6ed430a90d2ebaa2d39a10504e8306607c
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:26:41 +0500

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -1,40 +1,52 @@
 #include "Object.hpp"
 #include "TextureManager.hpp"
 
-Object::Object()
-{
-    Tex=NULL;
+// Default constructor for the Object class
+Object::Object() {
+    Tex = NULL; // Initialize the texture pointer to NULL
 }
 
-SDL_Texture* Object::getTexture()
-{
+// Getter function to retrieve the texture associated with the object
+SDL_Texture* Object::getTexture() {
     return Tex;
 }
-SDL_Rect& Object::getSrc()
-{
+
+// Getter function to retrieve the source rectangle
+SDL_Rect& Object::getSrc() {
     return src;
 }
-SDL_Rect& Object::getDest()
-{
+
+// Getter function to retrieve the destination rectangle
+SDL_Rect& Object::getDest() {
     return dest;
 }
 
-void Object::setSource(int x, int y, int w, int h)
-{
-    src.x=x;
-    src.y=y;
-    src.w=w;
-    src.h=h;
+// Setter function to set the source rectangle parameters
+// Parameters:
+//   x, y: Top-left corner coordinates of the source rectangle
+//   w, h: Width and height of the source rectangle
+void Object::setSource(int x, int y, int w, int h) {
+    src.x = x;
+    src.y = y;
+    src.w = w;
+    src.h = h;
 }
 
-void Object::setDest(int x, int y, int w, int h)
-{
-    dest.x=x;
-    dest.y=y;
-    dest.w=w;
-    dest.h=h;
+// Setter function to set the destination rectangle parameters
+// Parameters:
+//   x, y: Top-left corner coordinates of the destination rectangle
+//   w, h: Width and height of the destination rectangle
+void Object::setDest(int x, int y, int w, int h) {
+    dest.x = x;
+    dest.y = y;
+    dest.w = w;
+    dest.h = h;
 }
 
-void Object::CreateTexture(const char* address, SDL_Renderer* ren){
-    Tex=TextureManager::Texture(address, ren);
+// Function to load a texture from a file and set it as the object's texture
+// Parameters:
+//   address: File address of the texture image
+//   ren: SDL_Renderer used for rendering
+void Object::CreateTexture(const char* address, SDL_Renderer* ren) {
+    Tex = TextureManager::Texture(address, ren);
 }
