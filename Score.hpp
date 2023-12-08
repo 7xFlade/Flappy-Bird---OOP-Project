3cbfe2f2e23256592122b824d3a108d80284fe01
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:12:16 +0500

    Update Score.hpp

--- Score.hpp
+++ Score.hpp
@@ -1,14 +1,27 @@
-#pragma once 
+#pragma once
 
 class Score
 {
-    private:
-        double score=0;
-        double highscore=0;
-        
-    public:
-        void scoreInc();
-        void setHighscore(const double x);
-        double getScore() const;
-        double getHighschore() const;
+private:
+    double score = 0;    // Current score value
+    double highscore = 0; // Highest score achieved
+
+public:
+    // Function to increase the current score
+    void scoreInc();
+
+    // Function to set the highscore to a specific value
+    // Parameters:
+    //   x: The value to set as the new highscore
+    void setHighscore(const double x);
+
+    // Getter function to retrieve the current score
+    // Returns:
+    //   The current score value
+    double getScore() const;
+
+    // Getter function to retrieve the highscore
+    // Returns:
+    //   The highest score achieved
+    double getHighscore() const;
 };
