0994e0a693a274c680d2a55627888d352aba59db
Moiz Zulfiqar <122453916+7xFlade@users.noreply.github.com>
2023-12-09 02:28:27 +0500

    Update Nueron.hpp

--- Nueron.hpp
+++ Nueron.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
+// Neuron class representing a basic unit in a neural network
 class Neuron
 {
 private:
-	double value;
+    double value; // Value associated with the neuron
+
 public:
-	Neuron();
-	~Neuron();
-	void setValue(double val);
-	friend class NeuralNetwork;
+    // Default constructor for the Neuron class
+    Neuron();
+
+    // Destructor for the Neuron class
+    ~Neuron();
+
+    // Setter function to set the value of the neuron
+    // Parameters:
+    //   val: The value to set for the neuron
+    void setValue(double val);
+
+    // Friend class declaration for NeuralNetwork to allow access to private members
+    friend class NeuralNetwork;
 };
