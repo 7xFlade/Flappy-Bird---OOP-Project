740e1f0407759c72f54e36f2303e7f82e58e109c
SadCivic <122512802+SadCivic@users.noreply.github.com>
2023-12-09 08:51:59 +0500

    Update Score.cpp

--- Score.cpp
+++ Score.cpp
@@ -1,6 +1,7 @@
 #include "Score.hpp"
 
 //operator overloading obj++
+//here instead of using a function to update the score, we are simply making use of an overloaded iterator
 int Score::operator++(int)
 {
     int x=score+1;
@@ -15,6 +16,7 @@ void Score::setscore(const int x)
 }
 
 //if score>highscore
+//directly calling set high score function
 void Score::setHighscore(const int x){
     if (x>highscore){
         highscore=x;
@@ -26,7 +28,7 @@ int Score::getScore() const
 {
     return score;
 }
-        
+//following is to access and return highscore value
 int Score::getHighschore() const
 {
     return highscore;
